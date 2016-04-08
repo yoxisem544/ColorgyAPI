a703fe25f97254ba21cf19f8e116e4ca0464faa7
David Lin <yoxisem544@gmail.com>
2016-04-08 20:22:12 +0800

    refactor course raw object

--- ColorgyAPI/CourseRawDataObject.swift
+++ ColorgyAPI/CourseRawDataObject.swift
@@ -63,59 +63,6 @@ final public class CourseRawDataObject: CustomStringConvertible {
 	// new part
 	var general_code: String?
 	
-	// dictionary getter
-	var dictionary: [String : AnyObject] {
-		get {
-			var temp = [String : AnyObject]()
-			
-			// required
-			temp[rawDataKey.name] = name
-			temp[rawDataKey.code] = code
-			temp[rawDataKey.year] = year
-			temp[rawDataKey.term] = term
-			
-			// optional
-			temp[rawDataKey.credits] = credits
-			temp[rawDataKey.lecturer] = lecturer
-			// caution key is _type in json
-			temp[rawDataKey.type] = type
-			temp[rawDataKey.id] = id
-			
-			temp[rawDataKey.day_1] = day_1
-			temp[rawDataKey.day_2] = day_2
-			temp[rawDataKey.day_3] = day_3
-			temp[rawDataKey.day_4] = day_4
-			temp[rawDataKey.day_5] = day_5
-			temp[rawDataKey.day_6] = day_6
-			temp[rawDataKey.day_7] = day_7
-			temp[rawDataKey.day_8] = day_8
-			temp[rawDataKey.day_9] = day_9
-			temp[rawDataKey.period_1] = period_1
-			temp[rawDataKey.period_2] = period_2
-			temp[rawDataKey.period_3] = period_3
-			temp[rawDataKey.period_4] = period_4
-			temp[rawDataKey.period_5] = period_5
-			temp[rawDataKey.period_6] = period_6
-			temp[rawDataKey.period_7] = period_7
-			temp[rawDataKey.period_8] = period_8
-			temp[rawDataKey.period_9] = period_9
-			temp[rawDataKey.location_1] = location_1
-			temp[rawDataKey.location_2] = location_2
-			temp[rawDataKey.location_3] = location_3
-			temp[rawDataKey.location_4] = location_4
-			temp[rawDataKey.location_5] = location_5
-			temp[rawDataKey.location_6] = location_6
-			temp[rawDataKey.location_7] = location_7
-			temp[rawDataKey.location_8] = location_8
-			temp[rawDataKey.location_9] = location_9
-			
-			// new part
-			temp[rawDataKey.general_code] = general_code
-			
-			return temp
-		}
-	}
-	
 	public var description: String { return  "{\n\tname: \(name)\n\tcode: \(code)\n\tyear: \(year)\n\tterm: \(term)\n\tcredits: \(credits)\n\tlecturer: \(lecturer)\n\ttype: \(type)\n\tid: \(id)\n\tday_1: \(day_1)\n\tday_2: \(day_2)\n\tday_3: \(day_3)\n\tday_4: \(day_4)\n\tday_5: \(day_5)\n\tday_6: \(day_6)\n\tday_7: \(day_7)\n\tday_8: \(day_8)\n\tday_9: \(day_9)\n\tperiod_1: \(period_1)\n\tperiod_2: \(period_2)\n\tperiod_3: \(period_3)\n\tperiod_4: \(period_4)\n\tperiod_5: \(period_5)\n\tperiod_6: \(period_6)\n\tperiod_7: \(period_7)\n\tperiod_8: \(period_8)\n\tperiod_9: \(period_9)\n\tlocation_1: \(location_1)\n\tlocation_2: \(location_2)\n\tlocation_3: \(location_3)\n\tlocation_4: \(location_4)\n\tlocation_5: \(location_5)\n\tlocation_6: \(location_6)\n\tlocation_7: \(location_7)\n\tlocation_8: \(location_8)\n\tlocation_9: \(location_9)\n\tgeneral_code: \(general_code)\n}" }
 	
 	private struct rawDataKey {
@@ -169,141 +116,67 @@ final public class CourseRawDataObject: CustomStringConvertible {
 	/// You need to pass in a json, then will return a CourseRawDataObject?
 	///
 	/// :param: json: a json from server. This json is from **school API**, not user API.
-	public init?(json: JSON?) {
+	public init?(json: JSON) {
 		self.name = ""
 		self.code = ""
 		self.year = Int()
 		self.term = Int()
-		if let json = json {
-			// fail if this is a array of json
-			if !json.isArray {
-				// required part
-				if let name = json[rawDataKey.name].string {
-					self.name = name
-				} else {
-					return nil
-				}
-				if let code = json[rawDataKey.code].string {
-					self.code = code
-				} else {
-					return nil
-				}
-				// TODO: test if it will return int
-				if let year = json[rawDataKey.year].int {
-					self.year = year
-				} else {
-					return nil
-				}
-				if let term = json[rawDataKey.term].int {
-					self.term = term
-				} else {
-					return nil
-				}
-				// optional
-				if let credits = json[rawDataKey.credits].int {
-					self.credits = credits
-				}
-				if let lecturer = json[rawDataKey.lecturer].string {
-					self.lecturer = lecturer
-				}
-				if let type = json[rawDataKey.type].string {
-					self.type = type
-				}
-				if let id = json[rawDataKey.id].int {
-					self.id = id
-				}
-				// day, period, location
-				if let day_1 = json[rawDataKey.day_1].int {
-					self.day_1 = day_1
-				}
-				if let day_2 = json[rawDataKey.day_2].int {
-					self.day_2 = day_2
-				}
-				if let day_3 = json[rawDataKey.day_3].int {
-					self.day_3 = day_3
-				}
-				if let day_4 = json[rawDataKey.day_4].int {
-					self.day_4 = day_4
-				}
-				if let day_5 = json[rawDataKey.day_5].int {
-					self.day_5 = day_5
-				}
-				if let day_6 = json[rawDataKey.day_6].int {
-					self.day_6 = day_6
-				}
-				if let day_7 = json[rawDataKey.day_7].int {
-					self.day_7 = day_7
-				}
-				if let day_8 = json[rawDataKey.day_8].int {
-					self.day_8 = day_8
-				}
-				if let day_9 = json[rawDataKey.day_9].int {
-					self.day_9 = day_9
-				}
-				if let period_1 = json[rawDataKey.period_1].int {
-					self.period_1 = period_1
-				}
-				if let period_2 = json[rawDataKey.period_2].int {
-					self.period_2 = period_2
-				}
-				if let period_3 = json[rawDataKey.period_3].int {
-					self.period_3 = period_3
-				}
-				if let period_4 = json[rawDataKey.period_4].int {
-					self.period_4 = period_4
-				}
-				if let period_5 = json[rawDataKey.period_5].int {
-					self.period_5 = period_5
-				}
-				if let period_6 = json[rawDataKey.period_6].int {
-					self.period_6 = period_6
-				}
-				if let period_7 = json[rawDataKey.period_7].int {
-					self.period_7 = period_7
-				}
-				if let period_8 = json[rawDataKey.period_8].int {
-					self.period_8 = period_8
-				}
-				if let period_9 = json[rawDataKey.period_9].int {
-					self.period_9 = period_9
-				}
-				// location_
-				if let location_1 = json[rawDataKey.location_1].string {
-					self.location_1 = location_1
-				}
-				if let location_2 = json[rawDataKey.location_2].string {
-					self.location_2 = location_2
-				}
-				if let location_3 = json[rawDataKey.location_3].string {
-					self.location_3 = location_3
-				}
-				if let location_4 = json[rawDataKey.location_4].string {
-					self.location_4 = location_4
-				}
-				if let location_5 = json[rawDataKey.location_5].string {
-					self.location_5 = location_5
-				}
-				if let location_6 = json[rawDataKey.location_6].string {
-					self.location_6 = location_6
-				}
-				if let location_7 = json[rawDataKey.location_7].string {
-					self.location_7 = location_7
-				}
-				if let location_8 = json[rawDataKey.location_8].string {
-					self.location_8 = location_8
-				}
-				if let location_9 = json[rawDataKey.location_9].string {
-					self.location_9 = location_9
-				}
-				if let general_code = json[rawDataKey.general_code].string {
-					self.general_code = general_code
-				}
-			} else {
-				return nil
-			}
-		} else {
-			return nil
-		}
+		
+		// must not be an array
+		guard !json.isArray else { return nil }
+		
+		// required
+		guard let name = json[rawDataKey.name].string else { return nil }
+		self.name = name
+		
+		guard let code = json[rawDataKey.code].string else { return nil }
+		self.code = code
+		
+		guard let year = json[rawDataKey.year].int else { return nil }
+		self.year = year
+		
+		guard let term = json[rawDataKey.term].int else { return nil }
+		self.term = term
+		
+		
+		// optional
+		self.credits = json[rawDataKey.credits].int
+		self.lecturer = json[rawDataKey.lecturer].string
+		self.type = json[rawDataKey.type].string
+		self.id = json[rawDataKey.id].int
+		
+		
+		self.day_1 = json[rawDataKey.day_1].int
+		self.day_2 = json[rawDataKey.day_2].int
+		self.day_3 = json[rawDataKey.day_3].int
+		self.day_4 = json[rawDataKey.day_4].int
+		self.day_5 = json[rawDataKey.day_5].int
+		self.day_6 = json[rawDataKey.day_6].int
+		self.day_7 = json[rawDataKey.day_7].int
+		self.day_8 = json[rawDataKey.day_8].int
+		self.day_9 = json[rawDataKey.day_9].int
+		
+		self.period_1 = json[rawDataKey.period_1].int
+		self.period_2 = json[rawDataKey.period_2].int
+		self.period_3 = json[rawDataKey.period_3].int
+		self.period_4 = json[rawDataKey.period_4].int
+		self.period_5 = json[rawDataKey.period_5].int
+		self.period_6 = json[rawDataKey.period_6].int
+		self.period_7 = json[rawDataKey.period_7].int
+		self.period_8 = json[rawDataKey.period_8].int
+		self.period_9 = json[rawDataKey.period_9].int
+		
+		self.location_1 = json[rawDataKey.location_1].string
+		self.location_2 = json[rawDataKey.location_2].string
+		self.location_3 = json[rawDataKey.location_3].string
+		self.location_4 = json[rawDataKey.location_4].string
+		self.location_5 = json[rawDataKey.location_5].string
+		self.location_6 = json[rawDataKey.location_6].string
+		self.location_7 = json[rawDataKey.location_7].string
+		self.location_8 = json[rawDataKey.location_8].string
+		self.location_9 = json[rawDataKey.location_9].string
+		
+		self.general_code = json[rawDataKey.general_code].string
 	}
 	// 3. methods.
 	/// This method can get the counts of days, periods and locations array's length.
